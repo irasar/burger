3ba5930a7788c681bc0ec902615550c4402a593f
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-15 15:37:59 -0400

    requiring and exporting in burger.js

---
+++ models/burger.js
@@ -0,0 +1,11 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+
+
+
+
+
+
+// Export the database functions for the controller (burgers_Controller.js).
+module.exports = burger;
